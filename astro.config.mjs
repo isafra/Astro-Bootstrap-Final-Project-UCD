38faefe0808b2e17cc505e4f751d9c1f9fb4df65
Isabella Franca <isafra_89@icloud.com>
2024-07-31 20:58:31 +0100

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
-
+//import github from '@astrojs/github';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // adapter: staticSite(),
+   // site: 'https://isafra.github.io/Astro-Bootstrap-Final-Project-UCD/',
+    //base: 'Astro-Bootstrap-Final-Project-UCD',
+});
