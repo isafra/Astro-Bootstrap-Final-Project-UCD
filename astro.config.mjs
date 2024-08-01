dcbcd070785728c306d44c17d39a2ee2a25d0eee
Isabella Franca <isafra_89@icloud.com>
2024-08-01 23:48:57 +0100

    Api adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,7 @@
-//import { defineConfig } from 'astro/config';
-//import github from '@astrojs/github';
-// https://astro.build/config
-//export default defineConfig({
-    // adapter: staticSite(),
-  // site: 'https://isafra.github.io',
-   //base: '/Astro-Bootstrap-Final-Project-UCD/',
-//});
-import { defineConfig } from 'astro/config';
-//import staticAdapter from '@astrojs/adapter-static';
+import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://isafra.github.io',
   base: '/Astro-Bootstrap-Final-Project-UCD',
-  //adapter: staticAdapter(),
+  
 });
